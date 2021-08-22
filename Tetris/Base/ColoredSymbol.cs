e0ea402d1bf45118db51a59e698ef02ab5036745
Nicolas Wazulek <nicolas.wazulek@niwa-dev.de>
2021-08-22 18:32:14 +0200

    feat(base): added base types

---
+++ Tetris/Base/ColoredSymbol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Tetris.Base
+{
+    public class ColoredSymbol
+    {
+        public char Symbol { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public ColoredSymbol(char symbol, ConsoleColor color)
+        {
+            Symbol = symbol;
+            Color = color;
+        }
+    }
+}
---
+++ Tetris/Base/Direction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Tetris.Base
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
